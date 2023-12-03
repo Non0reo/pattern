baea411e0dfc9e7294165d2b9ac5fdfd6fa437e4
Non0reo <pieroni.nolhan@gmail.com>
2023-12-03 22:17:50 +0100

    Finished Patern Creation

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let img;
 let patternSize = {
-  width: 100,
-  height: 100
+  width: 80,
+  height: 80
 }
 let patterns = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  for (let x = 0; x * patternSize.width < width; x++) {
-    for (let y = 0; y * patternSize.height < height; y++) {
+  for (let x = 0; (x - 1) * patternSize.width < width; x++) {
+    for (let y = 0; (y - 1) * patternSize.height < height; y++) {
       const isPairColumn = x % 2 == 0;
       const isPairRow = y % 2 == 0;
       let squarePos = 0;
@@ -46,7 +46,7 @@ class Pattern {
   constructor(x, y, squarePos) {
     this.x = x;
     this.y = y;
-    this.rotation = int(random(4));
+    this.rotation = int(random(2));
     this.squarePos = squarePos;
   }
 
@@ -55,15 +55,25 @@ class Pattern {
       translate(this.x, this.y);
       //translate(this.x + patternSize.width / 2, this.y + patternSize.height / 2)
 
-      fill(255);
+      /* fill(255);
       textSize(20);
-      text(this.squarePos, 0, 0);
+      text(this.squarePos, 0, 0); */
       
       
       //rotate(this.rotation * (PI / 2));
 
+      switch (this.squarePos) {
+        case 0:
+        case 2:
+          rotate(this.rotation * PI);
+          break;
+        case 1:
+        case 3:
+          rotate((this.rotation * PI) + HALF_PI);
+          break;
+      }
       
-      image(img, 0, 0, patternSize.width, patternSize.height);
+      image(img, 0, 0, patternSize.width + 10, patternSize.height + 10);
       //image(img, 0, 0, 10, 10);
     pop();
   }
